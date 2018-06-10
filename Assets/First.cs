a43916035b28a1eb41e9b2d637359d12d43ce77b
BeatlesCoder <cwzhao1991@hotmaill.com>
2018-06-10 16:16:12 +0800

    Adjust code format.

--- Assets/First.cs
+++ Assets/First.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class First : MonoBehaviour {
+public class First : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-        int i = 0;
+    // Use this for initialization
+    void Start()
+    {
 
-        int j = 0;
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
